91f90d9f8adba0275a41a961e9da15fe8aef4589
EWilcox811 <ewilcox811@gmail.com>
2019-07-24 19:29:59 -0700

    Evan's Layouts
    Pulled Jeremy's layouts and added my own to push so we can all work from same project

---
+++ app/src/main/java/edu/nu/jam/capstone/ProgressiveSurvey.java
@@ -0,0 +1,14 @@
+package edu.nu.jam.capstone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProgressiveSurvey extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progressive_survey);
+    }
+}
---
+++ app/src/main/java/edu/nu/jam/capstone/ViewReplies.java
@@ -0,0 +1,14 @@
+package edu.nu.jam.capstone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewReplies extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reply_board);
+    }
+}
